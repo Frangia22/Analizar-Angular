6642bfa8b1bcbb646422a80e4395b25209a52254
Frangia22 <giacherofranc22@gmail.com>
2023-04-20 14:31:44 -0300

    exports pages

--- frontend/analizarFront/src/app/pages/pages.module.ts
+++ frontend/analizarFront/src/app/pages/pages.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ServiciosComponent,
+    FaqComponent,
+    ContactoComponent,
+    PresentacionComponent,
+    Pagina404Component
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports: [
+    ServiciosComponent,
+    FaqComponent,
+    ContactoComponent,
+    PresentacionComponent,
+    Pagina404Component
   ]
 })
 export class PagesModule { }
