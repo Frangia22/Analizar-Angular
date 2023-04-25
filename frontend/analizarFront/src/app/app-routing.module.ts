f70615806ca8ca3353aa01941fa60cf4f95b3035
Frangia22 <giacherofranc22@gmail.com>
2023-04-25 12:48:25 -0300

    Actualizo nav y home

--- frontend/analizarFront/src/app/app-routing.module.ts
+++ frontend/analizarFront/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { FaqComponent } from './pages/faq/faq.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { Pagina404Component } from './pages/pagina404/pagina404.component';
 import { PresentacionComponent } from './pages/presentacion/presentacion.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
@@ -12,12 +13,10 @@ const routes: Routes = [
   /*path: define la ruta virtual de nuestra aplicación.
     component: define el componente que le dice al enrutador que componente corresponde al seleccionar dicha ruta.
    */
-  {path: 'servicios', component:ServiciosComponent},
-  {path: 'faq', component:FaqComponent},
-  {path: 'contacto', component:ContactoComponent},
   {path: 'home', component:HomeComponent},
   {path: '', redirectTo:'/home', pathMatch:'full'},
-  {path: '**', component:Pagina404Component}
+  {path: 'login', component:LoginComponent},
+  {path: '**', component:Pagina404Component},
 ];
 
 @NgModule({
