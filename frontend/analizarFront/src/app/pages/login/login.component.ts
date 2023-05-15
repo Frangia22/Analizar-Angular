48695af6d84afa38060b14aff1c201ac07da438d
Frangia22 <giacherofranc22@gmail.com>
2023-05-15 16:51:25 -0300

    Acondiciono login para hacer la llamada a la api

--- frontend/analizarFront/src/app/pages/login/login.component.ts
+++ frontend/analizarFront/src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormGroup,  FormBuilder, FormControl, Validators, AbstractControl, NgFo
 import { Router } from '@angular/router';
 import { AuthService } from '../../service/auth.service';
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,7 +12,7 @@ import { AuthService } from '../../service/auth.service';
 export class LoginComponent implements OnInit {
   loginDash!: FormGroup;
   resetFormPass!: FormGroup;
-  //isLoggedIn!: boolean;
+  loginError: string = '';
   constructor( private fb: FormBuilder, private router: Router, private authService: AuthService) { }
 
   email: string = '';
@@ -20,16 +21,51 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginDash = this.initForm();
     this.resetFormPass = this.resetForm();  
+    /*this.authService.getData().subscribe((data: any) => {
+      for (let item of data) {
+        console.log(item.email);  
+      }
+      console.log(data); // Puedes hacer lo que necesites con los datos aquí
+    });*/
   }
   //Capturo los valores cuando le doy a ingresar
-  onSubmit(): void{    
+  onSubmit(){  
+    if(this.loginDash.valid){
+      this.authService.login(this.loginDash.value.email, this.loginDash.value.password).subscribe(  
+        (users: any[]) => {
+          //console.log(this.loginDash.value.email,this.loginDash.value.password);
+          //console.log(users);
+          const matchingUser = users.find(user => user.email == this.loginDash.value.email && user.password == this.loginDash.value.password);
+          //console.log(matchingUser);          
+          if (matchingUser) {
+            // Autenticación exitosa
+            //console.log('Yes');
+            localStorage.setItem('currentUser', JSON.stringify({ email: this.email }));
+            this.router.navigate(['/dashboard-client']);
+          } else {
+            // Datos de autenticación incorrectos
+            this.loginError = 'Usuario o contraseña incorrectos';
+            this.router.navigate(['/login']);
+            this.loginDash.reset();
+          }
+        },
+        (error) => {
+          // Manejar el error en caso de fallo en la petición
+        });
+    }else{
+      this.loginDash.markAllAsTouched();
+      this.loginError = 'Complete los campos';
+      //this.loginDash.reset()
+      //console.log('Lo siento fallo');
+    }
+    /*  
     if (this.authService.login(this.loginDash.value.email, this.loginDash.value.password)) {
       console.log(this.loginDash.value.email, this.loginDash.value.password);
       this.router.navigate(['/dashboard-client']);
     } else {
       console.log(this.loginDash.value.email, this.loginDash.value.password);
       this.router.navigate(['/login']);
-    }   
+    } */  
   }
   //Validaciones para los campos
   initForm(): FormGroup {
